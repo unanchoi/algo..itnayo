c8b0bfd0afee5c8648f5a9109983148e50a4a4b1
unanchoi <unanc2@gmail.com>
2024-01-20 00:01:06 +0900

    폴더 구조 변경

--- programmers/lv2/JadenCase_문자열_만들기.py
+++ programmers/lv2/JadenCase_문자열_만들기.py
@@ -1,4 +1,4 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/12951
+# 출처 : https://school.programmers.co.kr/learn/courses/30/lessons/12951
 
 def solution(s):
     answers = []
--- programmers/lv2/최솟값만들기.py
+++ programmers/lv2/최솟값만들기.py
@@ -1,3 +1,5 @@
+# 출처 : https://school.programmers.co.kr/learn/courses/30/lessons/12941
+
 def solution(A,B):
     answer = 0
 
